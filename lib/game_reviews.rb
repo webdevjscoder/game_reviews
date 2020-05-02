39efd400226837f879dfaf134eceff3ee0800859
webdevjscoder <cobianjuan19@gmail.com>
2020-05-02 09:38:39 -0700

    first commit

---
+++ lib/game_reviews.rb
@@ -0,0 +1,6 @@
+require "game_reviews/version"
+
+module GameReviews
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/game_reviews/version.rb
@@ -0,0 +1,3 @@
+module GameReviews
+  VERSION = "0.1.0"
+end
